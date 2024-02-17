27224cb644c433274b6a8a4862523c096d326ceb
Md jewel <jewel3472311@gmail.com>
2024-02-17 11:55:07 +0600

    added the rectangle calculate

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea(){
- console.log('calculate rect area')
+const lengthInput =document.getElementById('rectangle-length');
+const lengthText = lengthInput.value;
+const length =parseFloat(lengthText);
+console.log(length);
+
+
+const widthInput = document.getElementById('rectangle-width');
+const widthText =widthInput.value;
+const width = parseFloat(widthText);
+console.log(width);
+
+// calculate rectangle area
+const area = length * width;
+console.log('area of the rectangle:',area);
+
+// display rectangle area
+const rectangleAreaSpan = document.getElementById('rectangle-area')
+rectangleAreaSpan.innerText = area;
 }
--- script/triangle.js
+++ script/triangle.js
@@ -1,18 +1,38 @@
-function calculateTriangleArea(){
-const triangleBaseInput = document.getElementById('triangle-base');
-const triangleBaseText = triangleBaseInput.value;
-const base = parseFloat(triangleBaseText);
-console.log(base);
+// function calculateTriangleArea(){
+// const triangleBaseInput = document.getElementById('triangle-base');
+// const triangleBaseText = triangleBaseInput.value;
+// const base = parseFloat(triangleBaseText);
+// console.log(base);
 
-const triangleheightInput = document.getElementById('triangle-height');
-const triangleheightText =triangleheightInput.value;
-const height =parseFloat(triangleheightText);
-console.log(height);
-}
-// calculate triangle area
-const area = 0.5 * base * height;
-console.log('area of the triangle is:',area);
+// const triangleheightInput = document.getElementById('triangle-height');
+// const triangleheightText =triangleheightInput.value;
+// const height =parseFloat(triangleheightText);
+// console.log(height);
+// }
+// // calculate triangle area
+// const area = 0.5 * base * height;
+// console.log('area of the triangle is:',area);
 
-// display triangle area
-const triangleAreaSpan = document.getElementById('triangle-area');
-triangleAreaSpan.innerText = area;
+// // display triangle area
+// const triangleAreaSpan = document.getElementById('triangle-area');
+// triangleAreaSpan.innerText = area;
+function calculateRectangleArea(){
+ const triangleBaseInput =document.getElementById('rectangle-length');
+ const lengthText = lengthInput.value;
+ const length =parseFloat(lengthText);
+ console.log(length);
+ 
+ 
+ const widthInput = document.getElementById('rectangle-width');
+ const widthText =widthInput.value;
+ const width = parseFloat(widthText);
+ console.log(width);
+ 
+ // calculate rectangle area
+ const area = length * width;
+ console.log('area of the rectangle:',area);
+ 
+ // display rectangle area
+ const rectangleAreaSpan = document.getElementById('rectangle-area')
+ rectangleAreaSpan.innerText = area;
+ }
